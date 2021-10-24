a8596a8f519c6cf9cd9f616d137ece549e52d927
= <=>
2021-10-24 10:49:03 +0530

    small change

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,15 @@ module.exports = router;
 var textAWS = require("./../text-detection");
 
 
-/* Method - Detect face of users by photo.
-   Recive - Photo in format base 64.
-   Return - Return result of reconize.
-*/   
+   
 router.post('/detect-text', function(req, res){
     
-    //recive param photo in base64.
+  
     const obj = {
         photo: req.body.photo
     }
     
-    //calling method API AWS to recoknition face.
+   
     textAWS.detect_text(obj, function(data){
         
         if(data.found) res.send(data);
